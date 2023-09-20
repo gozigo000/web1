baa1ccf77c52381653813c12f03c08fb35c8b400
gozigo000 <143670404+gozigo000@users.noreply.github.com>
2023-09-20 19:33:28 +0900

    Add files via upload

---
+++ out-editor/vs/base/common/worker/simpleWorker.nls.ko.js
@@ -0,0 +1,41 @@
+/*!-----------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.0.0(86921c2fb801b4a0679dbab1b8ec6e1ae3e38421)
+ * Released under the MIT license
+ * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
+ *-----------------------------------------------------------*/
+
+define("vs/base/common/worker/simpleWorker.nls.ko", {
+	"vs/base/common/platform": [
+		"_",
+	],
+	"vs/editor/common/languages": [
+		"array",
+		"boolean",
+		"class",
+		"constant",
+		"constructor",
+		"enumeration",
+		"enumeration member",
+		"event",
+		"field",
+		"file",
+		"function",
+		"interface",
+		"key",
+		"method",
+		"module",
+		"namespace",
+		"null",
+		"number",
+		"object",
+		"operator",
+		"package",
+		"property",
+		"string",
+		"struct",
+		"type parameter",
+		"variable",
+		"{0} ({1})",
+	]
+});
